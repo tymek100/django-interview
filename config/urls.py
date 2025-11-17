913c07973e12128ab8c9f4d82b6f34f05bf57f82
Tymek <t.ilczyszyn@gmail.com>
2025-11-17 12:43:33 +0100

    changes

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("excel_summary.urls")),
+    
+    # OpenAPI schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
